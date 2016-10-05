c1984f0915aa5712f25ab45223af1b9cc09f096d
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-10-04 21:19:44 -0700

    Convert js to es6

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 require('./main.css');
 
-var Elm = require('./Main.elm');
+const Elm = require('./Main.elm');
 
-var root  = document.getElementById('elm-audioplayer');
+const root = document.getElementById('elm-audioplayer');
 
-var app = Elm.Main.embed(root);
+const app = Elm.Main.embed(root);
 
-app.ports.setCurrentTime.subscribe(function (time) {
-  var audio = document.getElementById('elm-audio-file');
+app.ports.setCurrentTime.subscribe((time) => {
+  const audio = document.getElementById('elm-audio-file');
   audio.currentTime = time;
 });
 
-app.ports.setPlaybackRate.subscribe(function(rate) {
-  var audio = document.getElementById('elm-audio-file');
+app.ports.setPlaybackRate.subscribe((rate) => {
+  const audio = document.getElementById('elm-audio-file');
   audio.playbackRate = rate;
 });
 
-app.ports.play.subscribe(function() {
-  var audio = document.getElementById('elm-audio-file');
+app.ports.play.subscribe(() => {
+  const audio = document.getElementById('elm-audio-file');
   audio.play();
 });
 
-app.ports.pause.subscribe(function() {
-  var audio = document.getElementById('elm-audio-file');
+app.ports.pause.subscribe(() => {
+  const audio = document.getElementById('elm-audio-file');
   audio.pause();
 });
 
