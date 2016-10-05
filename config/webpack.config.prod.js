638c46e924f4c20e555119c33d1594bd0ca8243e
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-10-04 20:20:44 -0700

    Update prod webpack setup

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,8 @@
+const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const paths = require('../config/paths');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const root = process.cwd();
@@ -8,7 +10,7 @@ const root = process.cwd();
 module.exports = {
   bail: true,
   entry: [
-    paths.entry
+    paths.entry,
   ],
   output: {
 
@@ -16,62 +18,73 @@ module.exports = {
     path: paths.dist,
 
     // Generated JS files.
-    filename: 'js/[name].[chunkhash:8].js'
+    filename: 'js/[name].[chunkhash:8].js',
   },
   resolveLoader: {
 
     // Look for loaders in own ./node_modules
     root: paths.ownModules,
-    moduleTemplates: [ '*-loader' ]
+    moduleTemplates: ['*-loader'],
   },
   resolve: {
-    modulesDirectories: [ 'node_modules' ],
-    extensions: [ '', '.js', '.elm' ]
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.elm'],
   },
   module: {
     noParse: /\.elm$/,
     loaders: [
       {
         test: /\.elm$/,
-        exclude: [ /elm-stuff/, /node_modules/ ],
+        exclude: [/elm-stuff/, /node_modules/],
 
         // Use the local installation of elm-make
         loader: 'elm-webpack',
         query: {
-          pathToMake: paths.elmMake
-        }
+          pathToMake: paths.elmMake,
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader: 'babel',
+        query: {
+          presets: ['react-app'],
+        },
       },
       {
         test: /\.css$/,
-        loader: 'style!css'
+        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss'),
+
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         exclude: /\/favicon.ico$/,
         loader: 'file',
         query: {
-          name: 'static/media/[name].[hash:8].[ext]'
-        }
-      }
-    ]
+          name: 'media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+  postcss: function postcss() {
+    return [
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9',
+        ],
+      }),
+    ];
   },
   plugins: [
 
     // Remove the content of the ./dist/ folder.
-    new CleanWebpackPlugin([ 'dist' ], {
-      root: root,
+    new CleanWebpackPlugin(['dist'], {
+      root,
       verbose: true,
-      dry: false
-    }),
-
-    // Minify the compiled JavaScript.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
+      dry: false,
     }),
 
     new HtmlWebpackPlugin({
@@ -88,8 +101,36 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
-      }
-    })
-  ]
+        minifyURLs: true,
+      },
+    }),
+
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify the compiled JavaScript.
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+      },
+      mangle: {
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+        screw_ie8: true,
+      },
+    }),
+
+    new ExtractTextPlugin('css/[name].[contenthash:8].css'),
+  ],
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
 };
