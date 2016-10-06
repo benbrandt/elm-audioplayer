08b157b7f0900b5513c7515d74f271139b11090b
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-10-05 21:39:40 -0700

    Add basic tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,16 +2,26 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
+import AudioPlayer
 
 
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Addition" <|
+        [ test "updatePlayhead" <|
             \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
+                AudioPlayer.updatePlayhead 7.308 9.135
+                    |> Expect.equal 80
+        , test "Time Formatting" <|
             \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
+                AudioPlayer.formatTime 3601
+                    |> Expect.equal "01:00:01"
+        , test "Time Formatting 2" <|
+            \() ->
+                AudioPlayer.formatTime 3599
+                    |> Expect.equal "59:59"
+        , test "Time Formatting 3" <|
+            \() ->
+                AudioPlayer.formatTime 360001
+                    |> Expect.equal "100:00:01"
         ]
